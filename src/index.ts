3c1bce54a473318df3b0c4aef0f84ca2cdd3c084
Adegbenga Oluwatosin <adegbengaoluwatosin61@gmail.com>
2026-08-04 16:13:42 +0100

    fix: CORS error

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.use(
 
 // CORS
 const corsOptions = {
-    origin: ["*", "http://localhost:3000", "http://localhost:5173"],
+    origin: ["https://wxyzwears.netlify.app", "http://localhost:3000", "http://localhost:5173"],
     credentials: true,
     optionSuccessStatus: 200,
 };
